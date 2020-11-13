f8a682ce969e4b64cff1f3005218db8157ce819c
DiBoS290699 <dimaborisov290699@yandex.ru>
2020-11-14 00:19:27 +0400

    decode() completed

--- Lab4/RMCode.py
+++ Lab4/RMCode.py
@@ -45,6 +45,43 @@ class RMCode:
         else:
             return np.dot(input_k, self.G_r_m)
 
+    def H_i_m(self, i, m):
+        if i < 1 or m < i:
+            print(f"ERROR! The value i must be 1 <= i <= m. i is {i}, m is {m}")
+            return None
+        else:
+            I_left = np.eye(2**(m - i))
+            H = np.array([[1, 1], [1, -1]])
+            I_right = np.eye(2**(i - 1))
+            return np.kron(np.kron(I_left, H), I_right)
+
+    def v_j(self, j):
+        str_j = bin(j)[2:]
+        list = np.zeros(self.m, dtype=bool)
+        for i in range(len(str_j)):
+            list[i] = int(str_j[-i - 1]) == 1
+        return list
+
+    def decode(self, input_n):
+        if len(input_n) != self.n:
+            print(f"ERROR! The input_n has an incorrect size. The size must be equal to {self.n} instead of {len(input_n)}")
+            return None
+        new_input_n = np.ones(self.n, dtype=int)
+        for i in range(self.n):
+            new_input_n[i] = 1 if input_n[i] == 1 else -1
+
+        w_i = np.dot(new_input_n, self.H_i_m(1, self.m))
+        for i in range(2, self.m + 1):
+            w_i = np.dot(w_i, self.H_i_m(i, self.m))
+        j = np.argmax(np.abs(w_i))
+        v_j = self.v_j(j)
+
+        result = np.zeros(len(v_j) + 1, dtype=bool)
+        for i in range(1, len(v_j) + 1):
+            result[i] = v_j[i - 1]
+        result[0] = w_i[j] > 0
+        return result
+
 
 rmc = RMCode(1, 3)
 # G_r_m = rmc.gen_matrix(3, 3)
@@ -54,5 +91,10 @@ for i in range(rmc.k):
     input_k.append(np.random.randint(0, 2, dtype=bool))
 ba_input_k = ba.bitarray(input_k)
 print(f"The input_k == {ba_input_k}")
-encode = rmc.encode([False, False, True, True])
+encode = rmc.encode(ba_input_k)
 print(f"The encode == {encode}")
+index_error = np.random.randint(0, len(encode))
+encode[index_error] = encode[index_error] is False
+print(f'The encode with an error in the {index_error} bit: {encode}')
+decode = rmc.decode(encode)
+print(f'The decode without errors: {decode}')
