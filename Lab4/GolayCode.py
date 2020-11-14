cb05c37ef0cc9b289f4eb440c3176f550ba4c7ca
DiBoS290699 <dimaborisov290699@yandex.ru>
2020-11-14 12:53:23 +0400

    Lab4 completed

--- Lab4/GolayCode.py
+++ Lab4/GolayCode.py
@@ -23,6 +23,7 @@ class GolayCode:
         ])
 
     def wt(self, input_signal):
+        # Получение веса кода
         wt = 0
         for i in range(len(input_signal)):
             if input_signal[i]:
@@ -30,55 +31,57 @@ class GolayCode:
         return wt
 
     def list2bitarray(self, list):
+        # Преобразование массива в bitarray
         ba_list = ba.bitarray('0'*len(list))
         for k in range(len(list)):
             ba_list[k] = list[k]
         return ba_list
 
     def encode(self, input):
+        # Кодировка сигнала
         if len(input) != self.k:
             print("Error!")
             return None
-        I = np.eye(self.k, dtype=bool)
-        G = np.concatenate([I, self.B], axis=1)
-        if input.__class__ == ba.bitarray().__class__:
-            np_input = np.array(input.tolist(), dtype=bool)
+        I = np.eye(self.k)  # Единичная матрица из нулей и единиц
+        G = np.concatenate([I, self.B], axis=1)     # Матрица G, состоящая из нулей и единиц (G = [I B])
+        if input.__class__ == ba.bitarray().__class__:  # Если передаваемый параметр - bitarray, то вызываем tolist()
+            np_input = np.array(input.tolist())
         else:
-            np_input = np.array(input, dtype=bool)
-        encode = np.dot(np_input, G)
-        return self.list2bitarray(encode)
+            np_input = np.array(input)
+        encode = np.dot(np_input, G) % 2
+        return np.array(encode, dtype=bool)     # Возвращение булевого массива, содержащего закодированный сигнал
 
     def decode(self, input_n):
-        I = np.eye(self.k, dtype=bool)
-        H = np.concatenate([I, self.B])
-
-        if input_n.__class__ == ba.bitarray().__class__:
-            np_input = np.array(input_n.tolist(), dtype=bool)
+        # Декодирование кода
+        I = np.eye(self.k)  # Единичная матрица из нулей и единиц
+        H = np.concatenate([I, self.B])     # Матрица H, состоящая из нулей и единиц (H = [ I ])
+        #                                                                                 [ B ]
+        if input_n.__class__ == ba.bitarray().__class__:   # Если передаваемый параметр - bitarray, то вызываем tolist()
+            np_input = np.array(input_n.tolist())
         else:
-            np_input = np.array(input_n, dtype=bool)
-        s = np.dot(np_input, H)                 # Step 1
+            np_input = np.array(input_n)
+        s = np.dot(np_input, H) % 2                 # Step 1
         wt_s = self.wt(s)
 
         if wt_s <= 3:                           # Step 2
-            u = np.zeros(self.n)
+            u = np.zeros(self.n, dtype=bool)
             for i in range(self.k):
                 u[i] = s[i]
             return np.logical_xor(np_input, u)
 
-        # ba_s = self.list2bitarray(s)
         for i in range(self.k):                 # Step 3
             tmp = np.logical_xor(s, self.B[i])
             if self.wt(tmp) <= 2:
-                u = np.zeros(self.n)
+                u = np.zeros(self.n, dtype=bool)
                 u[self.k + i] = True
                 for j in range(self.k):
                     u[j] = tmp[j]
                 return np.logical_xor(np_input, u)
 
-        sB = np.dot(s, self.B)                  # Step 4
+        sB = np.dot(s, self.B) % 2                  # Step 4
         wt_sB = self.wt(sB)
         if wt_sB <= 3:                          # Step 5
-            u = np.zeros(self.n)
+            u = np.zeros(self.n, dtype=bool)
             for i in range(self.n - self.k):
                 u[self.n - self.k + i] = s[i]
             return np.logical_xor(np_input, u)
@@ -86,7 +89,7 @@ class GolayCode:
         for i in range(self.k):                 # Step 6
             tmp = np.logical_xor(sB, self.B[i])
             if self.wt(tmp) <= 2:
-                u = np.zeros(self.n)
+                u = np.zeros(self.n, dtype=bool)
                 u[i] = True
                 for j in range(self.n - self.k):
                     u[self.k + j] = tmp[j]
@@ -96,16 +99,17 @@ class GolayCode:
         return None
 
 
-# gc = GolayCode()
-# input_k = ba.bitarray('011011111010')
-# encode = gc.encode(input_k)
-# print(f"Encode == {encode}")
-# index_error = 1
-# encode[index_error] = not encode[index_error]
+gc = GolayCode()
+input_k = ba.bitarray('011011111010')
+encode = gc.encode(input_k)
+print(f"Encode == {encode}")
+index_error = 1
+encode[index_error] = not encode[index_error]
 # print(f"Encode with an error in the {index_error} bit: 101111101111010010010010")
 # decode = gc.decode(ba.bitarray('101111101111010010010010'))
-# if decode is None:
-#    print('Decoding failed')
-# else:
-#    print(f'Decode == {decode}')
-
+print(f"Encode with an error in the {index_error} bit: {encode}")
+decode = gc.decode(encode)
+if decode is None:
+   print('Decoding failed')
+else:
+   print(f'Decode == {decode}')
--- Lab4/RMCode.py
+++ Lab4/RMCode.py
@@ -18,72 +18,79 @@ class RMCode:
         self.G_r_m = self.gen_matrix(r, m)
 
     def gen_matrix(self, r, m):
+        # Построение порождающей матрицы рекурсивным методом
         if m is None or r is None or r < 0 or m < 0 or m < r or m == r == 0:
             print(f"ERROR! The value m or r is incorrect: m is {m}, r is {r}")
             return None
         elif r == 0:
-            line = np.ones((1, 2**m), dtype=bool)
+            line = np.ones((1, 2**m), dtype=bool)   # Возвращает массив единиц с указанным размером
             return line
         elif r == m:
-            bottom_line = np.zeros((1, 2**m), dtype=bool)
-            bottom_line[0][-1] = True
-            return np.concatenate([self.gen_matrix(m - 1, m), bottom_line])
+            bottom_line = np.zeros((1, 2**m), dtype=bool)   # Возвращает массив нулей с указанным размером
+            bottom_line[0][-1] = True       # Последний бит равен True
+            return np.concatenate([self.gen_matrix(m - 1, m), bottom_line]) # Конкатенация массивов, при axis=0 (По умол.) - снизу, при axis=1 - справа
         else:
-            G_r_m_1 = np.concatenate([self.gen_matrix(r, m - 1), self.gen_matrix(r, m - 1)], axis=1)
+            G_r_m_1 = np.concatenate([self.gen_matrix(r, m - 1), self.gen_matrix(r, m - 1)], axis=1) # Конкатенация массивов, при axis=0 (По умол.) - снизу, при axis=1 - справа
             G_r_1_m_1 = self.gen_matrix(r - 1, m - 1)
-            shape_zeros = np.shape(G_r_1_m_1)
+            shape_zeros = np.shape(G_r_1_m_1)       # Получение размерности матрицы G
             zeros = np.zeros(shape_zeros, dtype=bool)
             return np.concatenate([G_r_m_1, np.concatenate([zeros, G_r_1_m_1], axis=1)])
 
     def encode(self, input_k):
+        # Кодирование, путём умножения на порождающую матрицу
         if self.k != len(input_k):
             print(f'ERROR! The input_k has an incorrect size. The size must be equal to {self.k} instead of {len(input_k)}')
             return None
-        if input_k.__class__ == ba.bitarray().__class__:
-            np_input_k = np.array(input_k.tolist(), dtype=bool)
-            return np.dot(np_input_k, self.G_r_m)
+        if input_k.__class__ == ba.bitarray().__class__:  # Если передаваемый параметр - bitarray, то вызываем tolist()
+            np_input_k = np.array(input_k.tolist(), dtype=int)
         else:
-            return np.dot(input_k, self.G_r_m)
+            np_input_k = np.array(input_k, dtype=int)
+        np_G_r_m = np.array(self.G_r_m, dtype=int)
+        dot = np.dot(np_input_k, np_G_r_m) % 2      # Умножение матриц и приведение к булевым значениям
+        return np.array(dot, dtype=bool)
 
     def H_i_m(self, i, m):
+        # Получение H^i_m с помощью умножения Кронекера
         if i < 1 or m < i:
             print(f"ERROR! The value i must be 1 <= i <= m. i is {i}, m is {m}")
             return None
         else:
-            I_left = np.eye(2**(m - i))
+            I_left = np.eye(2**(m - i))     # Единичная матрица определённого размера
             H = np.array([[1, 1], [1, -1]])
-            I_right = np.eye(2**(i - 1))
-            return np.kron(np.kron(I_left, H), I_right)
+            I_right = np.eye(2**(i - 1))    # Единичная матрица определённого размера
+            return np.kron(np.kron(I_left, H), I_right)  # Последовательное произведение Кронекера
 
     def v_j(self, j):
-        str_j = bin(j)[2:]
+        # Двоичное представление передаваемого числа с младшими разрядами слева
+        str_j = bin(j)[2:]  # Приведение числа к булевому значению (но хранится как строка) с пропуском символов формата
         list = np.zeros(self.m, dtype=bool)
-        for i in range(len(str_j)):
+        for i in range(len(str_j)):     # Получение реверсивного вида битового числа (младшие разряды слева)
             list[i] = int(str_j[-i - 1]) == 1
         return list
 
     def decode(self, input_n):
+        # Алгоритм декодирования
         if len(input_n) != self.n:
             print(f"ERROR! The input_n has an incorrect size. The size must be equal to {self.n} instead of {len(input_n)}")
             return None
         new_input_n = np.ones(self.n, dtype=int)
-        for i in range(self.n):
+        for i in range(self.n):     # Получение нового кода, в котором 0 заменяются на -1
             new_input_n[i] = 1 if input_n[i] == 1 else -1
 
-        w_i = np.dot(new_input_n, self.H_i_m(1, self.m))
+        w_i = np.dot(new_input_n, self.H_i_m(1, self.m))    # Получение w_1 путём умножение нового кода на H^1_m
         for i in range(2, self.m + 1):
-            w_i = np.dot(w_i, self.H_i_m(i, self.m))
-        j = np.argmax(np.abs(w_i))
-        v_j = self.v_j(j)
+            w_i = np.dot(w_i, self.H_i_m(i, self.m))        # Получение w_i путём умножение w_(i-1) на H^i_m
+        j = np.argmax(np.abs(w_i))      # Получение индекса максимального абсолютного значения в массиве
+        v_j = self.v_j(j)       # Получение реверсивного битового значения индекса j
 
         result = np.zeros(len(v_j) + 1, dtype=bool)
-        for i in range(1, len(v_j) + 1):
+        for i in range(1, len(v_j) + 1):        # Получение новго массива с True в начале при w_i[j] > 0, иначе False
             result[i] = v_j[i - 1]
         result[0] = w_i[j] > 0
         return result
 
-        # Возвращение кода со случайной ошибкой
     def make_a_mistake(self, code, count_errors=1):
+        # Возвращение кода со случайным количеством ошибок (максимум count_errors ошибок)
         if len(code) != self.n:
             print("ERROR! Invalid code length.")
             return code
@@ -93,12 +100,14 @@ class RMCode:
         return code
 
     def array2bitarray(self, array):
+        # Преобразование массива в bitarray
         ba_list = ba.bitarray('0'*len(array))
         for k in range(len(array)):
             ba_list[k] = array[k]
         return ba_list
 
     def encode_file(self, path_to_input_file, path_to_output_file, error=False):
+        # Кодировка файла из path_to_input_file в path_to_output_file с допуском ошибки (error is True) или без ошибок
         file_bitarray = ba.bitarray()
         with open(path_to_input_file, 'rb') as file:
             file_bitarray.fromfile(file)  # Чтение файла
@@ -122,6 +131,7 @@ class RMCode:
         print(f"Encoding ({path_to_input_file} in {path_to_output_file}) completed successfully")
 
     def decode_file(self, path_to_input_file, path_to_output_file):
+        # Декодирование файла path_to_input_file в файл path_to_output_file
         file_bitarray = ba.bitarray()
         with open(path_to_input_file, 'rb') as file:
             file_bitarray.fromfile(file)  # Чтение файла
@@ -144,22 +154,30 @@ for i in range(rmc.k):
     input_k.append(np.random.randint(0, 2, dtype=bool))
 ba_input_k = ba.bitarray(input_k)
 print(f"The input_k == {ba_input_k}")
+
 encode = rmc.encode(ba_input_k)
 print(f"The encode == {encode}")
+
 index_error = np.random.randint(0, len(encode))
 encode[index_error] = not encode[index_error]
 print(f'The encode with an error in the {index_error} bit: {encode}')
+
+# print(f'The encode: {10001111}')
+# decode = rmc.decode(ba.bitarray('10001111'))
 decode = rmc.decode(encode)
 print(f'The decode without errors: {decode}')
+
 path_to_input_file = "Hello.txt"
 path_to_output_file = "Hello_encode.txt"
 path_to_output_file_with_errors = "Hello_errors.txt"
 path_to_output_file_without_errors = "Hello_without_errors.txt"
+
 with open(path_to_input_file, 'wb') as file:
     file_bitarray = ba.bitarray('0'*rmc.k*3)   # создание bitarray с определённым количеством нулей
     for i in range(0, len(file_bitarray)):
         file_bitarray[i] = np.random.randint(0, 2) == 1     # Заполнение bitarray рандомными битами
     file_bitarray.tofile(file)                  # Запись псевдослучайный bitarray в файл
+
 rmc.encode_file(path_to_input_file, path_to_output_file, error=False)
 rmc.encode_file(path_to_input_file, path_to_output_file_with_errors, error=True)
 rmc.decode_file(path_to_output_file_with_errors, path_to_output_file_without_errors)
