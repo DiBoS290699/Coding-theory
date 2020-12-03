146156c99d1a063827f2f527b04024baaf65efbf
DiBoS290699 <dimaborisov290699@yandex.ru>
2020-12-03 15:42:48 +0400

    Lab5 v1.0 completed

--- Lab5/RMCode.py
+++ Lab5/RMCode.py
@@ -5,7 +5,7 @@ from itertools import combinations
 
 class RMCode:
 
-    def __init__(self, r, m):
+    def __init__(self, r, m, major=False):
         self.r = None if r < 0 else r
         self.m = None if m < 0 or m < r else m
         self.k = None
@@ -19,7 +19,7 @@ class RMCode:
         self.K_m = self.get_binary_digits()
         self.Z_m = np.arange(0, self.m)
         self.J_arr = self.J_array()
-        self.G_r_m = self.canon_G(self.J_arr, self.K_m)
+        self.G_r_m = self.canon_G(self.J_arr, self.K_m) if major else self.recur_G(self.r, self.m)
 
 
     def recur_G(self, r, m):
@@ -33,10 +33,10 @@ class RMCode:
         elif r == m:
             bottom_line = np.zeros((1, 2**m), dtype=bool)   # Возвращает массив нулей с указанным размером
             bottom_line[0][-1] = True       # Последний бит равен True
-            return np.concatenate([self.gen_matrix(m - 1, m), bottom_line]) # Конкатенация массивов, при axis=0 (По умол.) - снизу, при axis=1 - справа
+            return np.concatenate([self.recur_G(m - 1, m), bottom_line]) # Конкатенация массивов, при axis=0 (По умол.) - снизу, при axis=1 - справа
         else:
-            G_r_m_1 = np.concatenate([self.gen_matrix(r, m - 1), self.gen_matrix(r, m - 1)], axis=1) # Конкатенация массивов, при axis=0 (По умол.) - снизу, при axis=1 - справа
-            G_r_1_m_1 = self.gen_matrix(r - 1, m - 1)
+            G_r_m_1 = np.concatenate([self.recur_G(r, m - 1), self.recur_G(r, m - 1)], axis=1) # Конкатенация массивов, при axis=0 (По умол.) - снизу, при axis=1 - справа
+            G_r_1_m_1 = self.recur_G(r - 1, m - 1)
             shape_zeros = np.shape(G_r_1_m_1)       # Получение размерности матрицы G
             zeros = np.zeros(shape_zeros, dtype=bool)
             return np.concatenate([G_r_m_1, np.concatenate([zeros, G_r_1_m_1], axis=1)])
@@ -47,13 +47,40 @@ class RMCode:
             list.append(self.get_reverse_binary_number(i))
         return np.array(list)
 
+    def compare_J(self, J1, J2):
+        if len(J1) == len(J2):
+            for i in range(len(J1) - 1, -1, -1):
+                if J1[i] < J2[i]:
+                    return -1
+                elif J1[i] > J2[i]:
+                    return 1
+            return 0
+        elif len(J1) > len(J2):
+            return -1
+        else:
+            return 1
+
+    def quicksort_for_J_array(self, nums):
+        if len(nums) <= 1:
+            return nums
+        else:
+            q = nums[np.random.randint(0, len(nums))]
+        l_nums = [n for n in nums if self.compare_J(n, q) == -1]
+
+        e_nums = [q]
+        b_nums = [n for n in nums if self.compare_J(n, q) == 1]
+        return self.quicksort_for_J_array(l_nums) + e_nums + self.quicksort_for_J_array(b_nums)
+
     def J_array(self):
-        J_array = []
-        for i in range(self.r + 1):
+        J_array = [()]
+        if self.r > 0:
+            for j in range(self.m - 1, -1, -1):
+                J_array.append(tuple([j]))
+        for i in range(2, self.r + 1):
             tmp_list = []
             for j in combinations(self.Z_m, i):
                 tmp_list.append(j)
-            tmp_list = tmp_list[::-1]
+            tmp_list = self.quicksort_for_J_array(tmp_list)[::-1]
             J_array.extend(tmp_list)
         return np.array(J_array)
 
@@ -61,10 +88,7 @@ class RMCode:
         row = np.ones(len(bin_digits), dtype=bool)
         for j in range(len(bin_digits)):
             for k in J:
-                if k is None:
-                    row[j] &= True
-                else:
-                    row[j] &= not bin_digits[j, k]
+                row[j] &= not bin_digits[j, k]
         return row
 
     def canon_G(self, J_array, bin_digits):
@@ -72,7 +96,7 @@ class RMCode:
         for i in range(len(J_array)):
             row = self.row_canon_G(J_array[i], bin_digits)
             G_r_m.append(row)
-        return G_r_m
+        return np.array(G_r_m, dtype=bool)
 
     def encode(self, input_k):
         # Кодирование, путём умножения на порождающую матрицу
@@ -147,33 +171,30 @@ class RMCode:
     def major_decode(self, message):
         i = self.r
         w_i = np.array(message)
-        m = np.zeros(self.n, dtype=bool)
+        m = np.zeros(self.k, dtype=bool)
         sum_rows_with_1 = np.zeros(self.n, dtype=bool)
         for index_J in range(len(self.J_arr) - 1, -1, -1):
             J = self.J_arr[index_J]
-            if len(J) < i or J[-1] is None:
-                w_i = w_i + sum_rows_with_1
+            if len(J) < i:
+                w_i = w_i ^ sum_rows_with_1
+                sum_rows_with_1 = np.zeros(self.n, dtype=bool)
                 if not np.sum(w_i) > 2**(self.m - self.r - 1) - 1:
-                    print("w(i-1) has a weight of no more than 2**(m - r - 1) - 1)")
                     return m
                 else:
                     i -= 1
-                    index_J += 1
-                    continue
+            ver_vectors = self.verification_vectors_for_J(J)
+            dot_vv_with_w = []
+            for vector in ver_vectors.values():
+                dot_vv_with_w.append(self.dot_with_mod2(w_i, vector))
+            count_True = dot_vv_with_w.count(True)
+            count_False = len(dot_vv_with_w) - count_True
+            if count_True > count_False:
+                sum_rows_with_1 ^= self.row_canon_G(J, self.K_m)
+                m[index_J] = True
+            elif count_True < count_False:
+                m[index_J] = False
             else:
-                ver_vectors = self.verification_vectors_for_J(J)
-                dot_vv_with_w = []
-                for vector in ver_vectors.values():
-                    dot_vv_with_w.append(self.dot_with_mod2(w_i, vector))
-                count_True = dot_vv_with_w.count(True)
-                count_False = len(dot_vv_with_w) - count_True
-                if count_True > count_False:
-                    sum_rows_with_1 ^= self.verification_vectors_for_J(np.setdiff1d(self.Z_m, J))["[False False False False]"]
-                    m[index_J] = True
-                elif count_True < count_False:
-                    m[index_J] = False
-                else:
-                    raise Exception("Send the message again")
+                raise Exception("Send the message again")
         return m
 
 
@@ -204,10 +225,15 @@ class RMCode:
         if len(code) != self.n:
             print("ERROR! Invalid code length.")
             return code
-        for i in range(count_errors):
+        error_bits = []
+        i = 0
+        while i < count_errors:
             error_bit = np.random.randint(0, self.n)  # Индекс бита, в котором будет ошибка
-            code[error_bit] = not code[error_bit]
-        return code
+            if error_bit not in error_bits:
+                code[error_bit] = not code[error_bit]
+                error_bits.append(error_bit)
+                i += 1
+        return error_bits, code
 
     def array2bitarray(self, array):
         # Преобразование массива в bitarray
@@ -236,7 +262,7 @@ class RMCode:
                 if len(code) != self.k:
                     continue
                 encode = self.encode(code)
-                encode = self.make_a_mistake(encode, count_errors)
+                _, encode = self.make_a_mistake(encode, count_errors)
                 encode_file.extend(encode)
         with open(path_to_output_file, 'wb') as f:
             encode_file = self.array2bitarray(encode_file)
@@ -262,73 +288,22 @@ class RMCode:
         print(f"Decoding ({path_to_input_file} in {path_to_output_file}) completed successfully")
 
 
-rmc = RMCode(2, 4)
-message1 = [False, True, False, True, False, True, True, True, True, False, True, False,
-           False, False, False, False]
-message2 = [True, False, True, False, True, False, False, False, False, True, False,
-            True, True, True, True, True, ]
-decode = rmc.major_decode(message2)
-print(np.array(decode, dtype=int))
-# print(np.array(rmc.G_r_m, dtype=int))
-# G_r_m = rmc.gen_matrix(3, 3)
-# print(G_r_m)
-# input_k = []
-# for i in range(rmc.k):
-#     input_k.append(np.random.randint(0, 2, dtype=bool))
-# ba_input_k = ba.bitarray(input_k)
-# print(f"The input_k == \n{ba_input_k}")
-#
-# encode = rmc.encode(ba_input_k)
-# print(f"The encode == \n{np.array(encode, dtype=int)}")
-#
-# # print(f'The encode: {10001111}')
-# # decode = rmc.decode(ba.bitarray('10001111'))
-#
-# one_error = [np.random.randint(0, len(encode))]
-# two_errors = [0, 3]
-# three_errors = [1, 2, 4]
-# for_errors = [0, 5, 6, 7]
-#
-# tmp = rmc.encode(ba_input_k)
-# for i in one_error:
-#     tmp[i] = not tmp[i]
-# print(f'The encode with an error in the {one_error} bits: \n{np.array(tmp, dtype=int)}')
-# decode = rmc.decode(tmp)
-# print(f'The decode without errors: \n{np.array(decode, dtype=int)}')
-#
-# tmp = rmc.encode(ba_input_k)
-# for i in two_errors:
-#     tmp[i] = not tmp[i]
-# print(f'The encode with an error in the {two_errors} bits: \n{np.array(tmp, dtype=int)}')
-# decode = rmc.decode(tmp)
-# print(f'The decode without errors: \n{np.array(decode, dtype=int)}')
-#
-# tmp = rmc.encode(ba_input_k)
-# for i in three_errors:
-#     tmp[i] = not tmp[i]
-# print(f'The encode with an error in the {three_errors} bits: \n{np.array(tmp, dtype=int)}')
-# decode = rmc.decode(tmp)
-# print(f'The decode without errors: \n{np.array(decode, dtype=int)}')
-#
-# tmp = rmc.encode(ba_input_k)
-# for i in for_errors:
-#     tmp[i] = not tmp[i]
-# print(f'The encode with an error in the {for_errors} bits: \n{np.array(tmp, dtype=int)}')
-# decode = rmc.decode(tmp)
-# print(f'The decode without errors: \n{np.array(decode, dtype=int)}')
-#
-#
-# path_to_input_file = "Hello.txt"
-# path_to_output_file = "Hello_encode.txt"
-# path_to_output_file_with_errors = "Hello_errors.txt"
-# path_to_output_file_without_errors = "Hello_without_errors.txt"
-#
-# # with open(path_to_input_file, 'wb') as file:
-# #     file_bitarray = ba.bitarray('0'*rmc.k*3)   # создание bitarray с определённым количеством нулей
-# #     for i in range(0, len(file_bitarray)):
-# #         file_bitarray[i] = np.random.randint(0, 2) == 1     # Заполнение bitarray рандомными битами
-# #     file_bitarray.tofile(file)                  # Запись псевдослучайный bitarray в файл
-#
-# rmc.encode_file(path_to_input_file, path_to_output_file, error=False)
-# rmc.encode_file(path_to_input_file, path_to_output_file_with_errors, error=True)
-# rmc.decode_file(path_to_output_file_with_errors, path_to_output_file_without_errors)
+r = 4
+m = 4
+count_errors = 0
+rmc = RMCode(r, m, major=True)
+print(f"The canonical matrix G({r},{m})\n {rmc.G_r_m.astype(int)}")
+message = np.zeros(rmc.k, dtype=bool)
+for i in range(len(message)):
+    message[i] = np.random.randint(0, 2, dtype=bool)
+print(f"Message:\n {message.astype(int)}")
+encode = rmc.encode(message)
+print(f"encode:\n {encode.astype(int)}")
+indexes_error, encode = rmc.make_a_mistake(encode, count_errors=count_errors)
+print(f"encode with error in {indexes_error} bit:\n {encode.astype(int)}")
+try:
+    decode = rmc.major_decode(encode)
+    print(f"decode:\n {decode.astype(int)}")
+    print(f"message == decode:\n {np.equal(message, decode)}")
+except Exception:
+    print("ERROR! Send the message again")
