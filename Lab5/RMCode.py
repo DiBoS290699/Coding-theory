f32b5e039b02f191e420209a1f968f76c87a62c8
DiBoS290699 <dimaborisov290699@yandex.ru>
2020-12-03 23:48:32 +0400

    Lab5 - Patch 1.0.1 - added comments and minor fixes

--- Lab5/RMCode.py
+++ Lab5/RMCode.py
@@ -6,21 +6,194 @@ from itertools import combinations
 class RMCode:
 
     def __init__(self, r, m, major=False):
-        self.r = None if r < 0 else r
-        self.m = None if m < 0 or m < r else m
-        self.k = None
-        if self.r is not None and self.m is not None:
-            self.k = 0
-            for i in range(r + 1):
-                self.k += np.math.factorial(m)/(np.math.factorial(i)*np.math.factorial(m - i))
-            self.k = int(self.k)
-        self.n = None if m is None else 2**m
-        self.d = None if m is None or r is None else 2**(self.m - self.r)
-        self.K_m = self.get_binary_digits()
-        self.Z_m = np.arange(0, self.m)
-        self.J_arr = self.J_array()
+        if r < 0 or m < 0 or m < r:
+            raise ValueError(f"The incorrect values r and m: r = {r}, m = {m}")
+        self.r = r
+        self.m = m
+        self.k = 0
+        for i in range(r + 1):
+            self.k += np.math.factorial(m)/(np.math.factorial(i)*np.math.factorial(m - i))
+        self.k = int(self.k)
+        self.n = 2**m
+        self.d = 2**(self.m - self.r)
+        self.K_m = self.get_binary_digits(self.n)
+        self.Z_m = np.arange(0, self.m)     # Массив чисел от 0 до m-1
+        self.J_arr = self.J_array()     # Массив из J, где J включено в Z_m и |J| <= r
+        self.major = major  # major == True - вызывается конструктор матрицы G канонического вида, иначе через рекурсию
         self.G_r_m = self.canon_G(self.J_arr, self.K_m) if major else self.recur_G(self.r, self.m)
 
+    """Получение двоичного представления передаваемого числа с младшими разрядами слева"""
+    def get_reverse_binary_number(self, j):
+        str_j = bin(j)[2:]  # Приведение числа к булевому значению (но хранится как строка) с пропуском символов формата
+        list = np.zeros(self.m, dtype=bool)
+        for i in range(len(str_j)):     # Получение реверсивного вида битового числа (младшие разряды слева)
+            list[i] = int(str_j[-i - 1]) == 1
+        return list
+
+    """Получение списка двоичных представлений для разрядов от 0 до n = 2**m"""
+    def get_binary_digits(self, n):
+        list = []
+        for i in range(0, n):
+            list.append(self.get_reverse_binary_number(i))
+        return np.array(list)
+
+    """Получение списка подмножеств J, где J включено в Z_m и |J| <= r"""
+    def J_array(self):
+        J_array = [()]
+        if self.r > 0:
+            for j in range(self.m - 1, -1, -1):
+                J_array.append(tuple([j]))
+        for i in range(2, self.r + 1):
+            tmp_list = []
+            for j in combinations(self.Z_m, i):
+                tmp_list.append(j)
+            tmp_list = self.quicksort_for_J_array(tmp_list)[::-1]
+            J_array.extend(tmp_list)
+        return np.array(J_array)
+
+    """
+    Сравнение двух подмножеств J1 и J2
+    Возвращаемые значения:
+        -1 если J1 < J2
+        0 если J1 == J2
+        1 если J1 > J2
+    """
+    def compare_J(self, J1, J2):
+        if len(J1) == len(J2):
+            # Так как подмножества J пресдтавленны в виде упорядоченного списка, обход начинается со старших элементов
+            for i in range(len(J1) - 1, -1, -1):
+                if J1[i] < J2[i]:       # Если наибольший элемент J2 больше наиб. элемента J1, то J1 < J2
+                    return -1
+                elif J1[i] > J2[i]:     # Если наиб. элемент J2 меньше наиб. элемента J1, то J1 > J2
+                    return 1            # Если наиб. элементы равны, то переходим к следующему наиб. элементу
+            return 0                    # Если не был произведён return до этого момента, то J1 == J2
+        elif len(J1) > len(J2):
+            return -1   # Подмножество с меньшей размерностью, чем у другого, главнее
+        else:
+            return 1    # Подмножество с меньшей размерностью, чем у другого, главнее
+
+    """Быстрая сортировка массива J"""
+    def quicksort_for_J_array(self, nums):
+        if len(nums) <= 1:
+            return nums
+        else:
+            q = nums[np.random.randint(0, len(nums))]
+        l_nums = [n for n in nums if self.compare_J(n, q) == -1]
+
+        e_nums = [q]
+        b_nums = [n for n in nums if self.compare_J(n, q) == 1]
+        return self.quicksort_for_J_array(l_nums) + e_nums + self.quicksort_for_J_array(b_nums)
+
+    """
+    Получение строки матрицы G каноничным способом
+    Алгоритм:
+        Значение элемента матрицы равно 1, если в двоичном представлении
+        соотвествующего столбца все разряды, входящие в подмножество
+        соответствующего столбца, равны нулю.
+    """
+    def row_canon_G(self, J, bin_digits):
+        row = np.ones(len(bin_digits), dtype=bool)  # Изначально строка будет состоять только из True
+        for j in range(len(bin_digits)):
+            for k in J:
+                row[j] &= not bin_digits[j, k]
+        return row
+
+    """Получение матрицы G каноничным способом"""
+    def canon_G(self, J_array, bin_digits):
+        G_r_m = []
+        for i in range(len(J_array)):
+            row = self.row_canon_G(J_array[i], bin_digits)
+            G_r_m.append(row)
+        return np.array(G_r_m, dtype=bool)
+
+    """
+    Получение десятичного числа из двоичного представления
+    ( Выполняет обратное действие методу get_reverse_binary_number )
+    Передаваемые значения:
+        bits - любой массив битов
+        reverse - означает с какой стороны находятся младшие разряды (True - слева, False - справа)
+    """
+    def get_number_from_binary(self, bits, reverse=False):
+        number = 0
+        indexes_of_bits = range(len(bits)) if not reverse else range(len(bits) - 1, -1, -1)
+        degree = 0
+        for i in indexes_of_bits:   # Стандартное преобразование двоичного числа в десятичное число
+            if bits[i]:
+                number += 2**degree
+            degree += 1
+        return number
+
+    """
+    Получение всех допустимых двоичных представлений
+    сдвигов (в лекциях t), присвоив нули разрядам, входящим в множество J.
+    """
+    def get_binary_shifts(self, J):
+        t = self.K_m.copy()
+        if len(J) == 0:
+            return t
+        else:
+            for elem_t in t:
+                for elem_J in J:
+                    elem_t[elem_J] = False
+            return np.unique(t, axis=0)  # Удаление копий
+
+    """
+    Получение проверочные векторы (в лекциях v(Jc,t)) из 
+    базового посредством сдвига влево (интерпретируя элементы из 
+    множества t как двоичные представления сдвигов)
+    Возвращаемое значение:
+        Словарь пров. векторов, где ключ - сдвиг, значение - вектор со сдвигом
+    """
+    def verification_vectors_for_J(self, J):
+        J_c = np.setdiff1d(self.Z_m, J)     # Получение комплементарного подмножества J_c = Z_m \ J
+        b = self.row_canon_G(J_c, self.K_m)     # Базовый вектор, соответсвующий J_c
+        t = self.get_binary_shifts(J)       # Получение двоичных представлений сдвигов
+        ver_vectors = {}        # Словарь пров. векторов, где ключ - сдвиг, значение - вектор со сдвигом
+        for elem_t in t:
+            # np.roll() - операция сдвига элементов списка b на shift элементов
+            ver_vectors[str(elem_t)] = np.roll(b, shift=self.get_number_from_binary(elem_t))
+        return ver_vectors
+
+    """Скалярное произведение двоичных векторов со сложением по модулю 2 (XOR)"""
+    def dot_with_mod2(self, a, b):
+        if len(a) != len(b):
+            raise Exception("The dimensions of the transmitted values do not match")
+        else:
+            result = False
+            for i in range(len(a)):
+                result ^= a[i] & b[i]
+            return result
+
+    """Выполнение алгоритма мажоритарного декодирования"""
+    def major_decode(self, message):
+        i = self.r      # Текущая размерность подмножеств J
+        w_i = np.array(message)     # Битовая последовательность, которая будет изменяться, согласно значению i
+        m = np.zeros(self.k, dtype=bool)    # Декодированное сообщение (изначально полностью забит нулями)
+        sum_rows_with_1 = np.zeros(self.n, dtype=bool)      # Сумма всех m_J*v_J, где |J| = i
+        for index_J in range(len(self.J_arr) - 1, -1, -1):
+            J = self.J_arr[index_J]     # Текущее подмножество J
+            if len(J) < i:      # Если текущий J, размерностью меньше i, то переходим к i-1 меняем w(i) на w(i-1)
+                w_i = w_i ^ sum_rows_with_1
+                sum_rows_with_1 = np.zeros(self.n, dtype=bool)
+                if not np.sum(w_i) > 2**(self.m - self.r - 1) - 1:
+                    # Если вес w(i) не больше 2**(m-r-1) - 1, то останавливаемся
+                    return m
+                else:
+                    i -= 1
+            ver_vectors = self.verification_vectors_for_J(J)
+            dot_vv_with_w = []
+            for vector in ver_vectors.values():
+                dot_vv_with_w.append(self.dot_with_mod2(w_i, vector))
+            count_True = dot_vv_with_w.count(True)
+            count_False = len(dot_vv_with_w) - count_True
+            if count_True > count_False:
+                sum_rows_with_1 ^= self.row_canon_G(J, self.K_m)
+                m[index_J] = True
+            elif count_True < count_False:
+                m[index_J] = False
+            else:
+                raise Exception("Send the message again")
+        return m
 
     def recur_G(self, r, m):
         # Построение порождающей матрицы рекурсивным методом
@@ -40,63 +213,6 @@ class RMCode:
             shape_zeros = np.shape(G_r_1_m_1)       # Получение размерности матрицы G
             zeros = np.zeros(shape_zeros, dtype=bool)
             return np.concatenate([G_r_m_1, np.concatenate([zeros, G_r_1_m_1], axis=1)])
-
-    def get_binary_digits(self):
-        list = []
-        for i in range(0, self.n):
-            list.append(self.get_reverse_binary_number(i))
-        return np.array(list)
-
-    def compare_J(self, J1, J2):
-        if len(J1) == len(J2):
-            for i in range(len(J1) - 1, -1, -1):
-                if J1[i] < J2[i]:
-                    return -1
-                elif J1[i] > J2[i]:
-                    return 1
-            return 0
-        elif len(J1) > len(J2):
-            return -1
-        else:
-            return 1
-
-    def quicksort_for_J_array(self, nums):
-        if len(nums) <= 1:
-            return nums
-        else:
-            q = nums[np.random.randint(0, len(nums))]
-        l_nums = [n for n in nums if self.compare_J(n, q) == -1]
-
-        e_nums = [q]
-        b_nums = [n for n in nums if self.compare_J(n, q) == 1]
-        return self.quicksort_for_J_array(l_nums) + e_nums + self.quicksort_for_J_array(b_nums)
-
-    def J_array(self):
-        J_array = [()]
-        if self.r > 0:
-            for j in range(self.m - 1, -1, -1):
-                J_array.append(tuple([j]))
-        for i in range(2, self.r + 1):
-            tmp_list = []
-            for j in combinations(self.Z_m, i):
-                tmp_list.append(j)
-            tmp_list = self.quicksort_for_J_array(tmp_list)[::-1]
-            J_array.extend(tmp_list)
-        return np.array(J_array)
-
-    def row_canon_G(self, J, bin_digits):
-        row = np.ones(len(bin_digits), dtype=bool)
-        for j in range(len(bin_digits)):
-            for k in J:
-                row[j] &= not bin_digits[j, k]
-        return row
-
-    def canon_G(self, J_array, bin_digits):
-        G_r_m = []
-        for i in range(len(J_array)):
-            row = self.row_canon_G(J_array[i], bin_digits)
-            G_r_m.append(row)
-        return np.array(G_r_m, dtype=bool)
 
     def encode(self, input_k):
         # Кодирование, путём умножения на порождающую матрицу
@@ -122,84 +238,7 @@ class RMCode:
             I_right = np.eye(2**(i - 1))    # Единичная матрица определённого размера
             return np.kron(np.kron(I_left, H), I_right)  # Последовательное произведение Кронекера
 
-    def get_reverse_binary_number(self, j):
-        # Двоичное представление передаваемого числа с младшими разрядами слева
-        str_j = bin(j)[2:]  # Приведение числа к булевому значению (но хранится как строка) с пропуском символов формата
-        list = np.zeros(self.m, dtype=bool)
-        for i in range(len(str_j)):     # Получение реверсивного вида битового числа (младшие разряды слева)
-            list[i] = int(str_j[-i - 1]) == 1
-        return list
-
-    def get_number_from_binary(self, bits, reverse=False):
-        number = 0
-        indexes_of_bits = range(len(bits)) if not reverse else range(len(bits) - 1, -1, -1)
-        degree = 0
-        for i in indexes_of_bits:
-            if bits[i]:
-                number += 2**degree
-            degree += 1
-        return number
-
-    def get_binary_shifts(self, J):
-        t = self.K_m.copy()
-        if len(J) == 0:
-            return t
-        else:
-            for elem_t in t:
-                for elem_J in J:
-                    elem_t[elem_J] = False
-            return np.unique(t, axis=0)
-
-    def verification_vectors_for_J(self, J):
-        J_c = np.setdiff1d(self.Z_m, J)
-        b = self.row_canon_G(J_c, self.K_m)
-        t = self.get_binary_shifts(J)
-        ver_vectors = {}
-        for elem_t in t:
-            ver_vectors[str(elem_t)] = np.roll(b, shift=self.get_number_from_binary(elem_t))
-        return ver_vectors
-
-    def dot_with_mod2(self, a, b):
-        if len(a) != len(b):
-            raise Exception("The dimensions of the transmitted values do not match")
-        else:
-            result = False
-            for i in range(len(a)):
-                result ^= a[i] & b[i]
-            return result
-
-    def major_decode(self, message):
-        i = self.r
-        w_i = np.array(message)
-        m = np.zeros(self.k, dtype=bool)
-        sum_rows_with_1 = np.zeros(self.n, dtype=bool)
-        for index_J in range(len(self.J_arr) - 1, -1, -1):
-            J = self.J_arr[index_J]
-            if len(J) < i:
-                w_i = w_i ^ sum_rows_with_1
-                sum_rows_with_1 = np.zeros(self.n, dtype=bool)
-                if not np.sum(w_i) > 2**(self.m - self.r - 1) - 1:
-                    return m
-                else:
-                    i -= 1
-            ver_vectors = self.verification_vectors_for_J(J)
-            dot_vv_with_w = []
-            for vector in ver_vectors.values():
-                dot_vv_with_w.append(self.dot_with_mod2(w_i, vector))
-            count_True = dot_vv_with_w.count(True)
-            count_False = len(dot_vv_with_w) - count_True
-            if count_True > count_False:
-                sum_rows_with_1 ^= self.row_canon_G(J, self.K_m)
-                m[index_J] = True
-            elif count_True < count_False:
-                m[index_J] = False
-            else:
-                raise Exception("Send the message again")
-        return m
-
-
-
-    def decode(self, input_n):
+    def recur_decode(self, input_n):
         # Алгоритм декодирования
         if len(input_n) != self.n:
             print(f"ERROR! The input_n has an incorrect size. The size must be equal to {self.n} instead of {len(input_n)}")
@@ -280,7 +319,7 @@ class RMCode:
             code = file_bitarray[i: i + self.n]
             if len(code) != self.n:
                 continue
-            decode = self.decode(code)
+            decode = self.major_decode(code) if self.major else self.recur_decode(code)
             decode_file.extend(decode)
         with open(path_to_output_file, 'wb') as f:
             decode_file = self.array2bitarray(decode_file)
@@ -288,22 +327,32 @@ class RMCode:
         print(f"Decoding ({path_to_input_file} in {path_to_output_file}) completed successfully")
 
 
-r = 4
+r = 2
 m = 4
-count_errors = 0
-rmc = RMCode(r, m, major=True)
-print(f"The canonical matrix G({r},{m})\n {rmc.G_r_m.astype(int)}")
-message = np.zeros(rmc.k, dtype=bool)
-for i in range(len(message)):
-    message[i] = np.random.randint(0, 2, dtype=bool)
-print(f"Message:\n {message.astype(int)}")
-encode = rmc.encode(message)
-print(f"encode:\n {encode.astype(int)}")
-indexes_error, encode = rmc.make_a_mistake(encode, count_errors=count_errors)
-print(f"encode with error in {indexes_error} bit:\n {encode.astype(int)}")
+count_errors = 1
+
+rmc = None
 try:
-    decode = rmc.major_decode(encode)
-    print(f"decode:\n {decode.astype(int)}")
-    print(f"message == decode:\n {np.equal(message, decode)}")
-except Exception:
-    print("ERROR! Send the message again")
+    rmc = RMCode(r, m, major=True)
+except ValueError as e:
+    print(e)
+else:
+    print(f"The canonical matrix G({r},{m})\n {rmc.G_r_m.astype(int)}")
+
+    message = np.zeros(rmc.k, dtype=bool)
+    for i in range(len(message)):
+        message[i] = np.random.randint(0, 2, dtype=bool)
+    print(f"Message:\n {message.astype(int)}")
+
+    encode = rmc.encode(message)
+    print(f"encode:\n {encode.astype(int)}")
+
+    indexes_error, encode = rmc.make_a_mistake(encode, count_errors=count_errors)
+    print(f"encode with error in {indexes_error} bit:\n {encode.astype(int)}")
+
+    try:
+        decode = rmc.major_decode(encode)
+        print(f"decode:\n {decode.astype(int)}")
+        print(f"message == decode:\n {np.equal(message, decode)}")
+    except Exception:
+        print("ERROR! Send the message again")
